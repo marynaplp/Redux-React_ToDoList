3d70ddba20dd2938e9132b051baf7b43a65900cd
Maryna <maryna.pylypchenko@gmail.com>
2020-11-05 16:28:12 +0200

    commit

--- task1/src/tasks/components/ToDoList.jsx
+++ task1/src/tasks/components/ToDoList.jsx
@@ -1,46 +1,45 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import * as tasksActions from "../tasks.actions";
 import TasksList from "./TasksList";
-import * as tasksAction from "../tasks.actions"
-import { sortedTasksListSelector } from "../tasks.selectors"
-import { connect } from "react-redux"
-import PropTypes from "prop-types"
+import { sortedTasksListSelector } from "../tasks.selectors";
+
 class TodoList extends Component {
-    componentDidMount(){
-        this.props.getTaskList()
+    componentDidMount() {
+        this.props.getTaskList();
     }
-    render(){
-  return(
-    <>
-    <h1 className="title">Todo List</h1>
-    <TasksList 
-    tasks = {this.props.tasks}
-    onCreate = {this.props.createTask}
-    onDelete = {this.props.deleteTask}
-    onChange = {this.props.updateTask}
-    />
-    </>
-  )
-}
-}
-// TodoList.propTypes = {
-//   tasks: PropTypes.array(PropTypes.shape()),
-//   updateTask:PropTypes.func.isRequired,
-//   getTaskList: PropTypes.func.isRequired,
-//   createTask:PropTypes.func.isRequired,
-//   deleteTask:PropTypes.func.isRequired
 
-// }
-
-const mapDispatch ={
-getTasksList: tasksAction.getTasksList,
-updateTask: tasksAction.updateTask,
-createTask:tasksAction.createTask,
-deleteTask: tasksAction.deleteTask, 
-
+    render() {
+        return (
+            <>
+                <h1 className="title">Todo List</h1>
+                <TasksList
+                    tasks={this.props.tasks}
+                    onCreate={this.props.createTask}
+                    onDelete={this.props.deleteTask}
+                    onChange={this.props.updateTask}
+                />
+            </>
+        );
+    }
 }
+TodoList.propTypes = {
+    tasks: PropTypes.arrayOf(PropTypes.shape()),
+    getTaskList: PropTypes.func.isRequired,
+    updateTask: PropTypes.func.isRequired,
+    deleteTask: PropTypes.func.isRequired,
+    createTask: PropTypes.func.isRequired,
+};
+const mapDispatch = {
+    getTaskList: tasksActions.getTaskList,
+    updateTask: tasksActions.updateTask,
+    deleteTask: tasksActions.deleteTask,
+    createTask: tasksActions.createTask,
+};
 const mapState = (state) => {
     return {
         tasks: sortedTasksListSelector(state),
     };
 };
-export default connect(mapState, mapDispatch)(TodoList)
+export default connect(mapState, mapDispatch)(TodoList);
